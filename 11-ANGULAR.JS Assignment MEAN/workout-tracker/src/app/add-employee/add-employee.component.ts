17aacaf750f619646376ea0c90e0b60de00511a3
kirankn8 <kirankn8@gmail.com>
2018-08-24 10:12:18 +0530

    Add 11-ANGULAR.JS Assignment MEAN

---
+++ 11-ANGULAR.JS Assignment MEAN/workout-tracker/src/app/add-employee/add-employee.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee.model';
+import { EmployeeService } from '../services/employee.service';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.css']
+})
+export class AddEmployeeComponent implements OnInit {
+
+  employee: Employee;
+
+  constructor(
+    private employeeService: EmployeeService,
+    private location: Location) {
+    this.employee = {
+      name: '', age: null, email: ''
+    };
+  }
+
+  ngOnInit() {
+  }
+
+  onSave() {
+    this.employeeService.addEmployee(this.employee);
+    this.location.back();
+  }
+
+  onCancel() {
+    this.location.back();
+  }
+}
+
