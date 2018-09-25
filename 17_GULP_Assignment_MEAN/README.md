52c836f272df123a507b97cd72f0b1e9a8b4f10b
kirankn8 <kirankn8@gmail.com>
2018-09-25 10:02:28 +0530

    Complete Assignment 17

---
+++ 17_GULP_Assignment_MEAN/README.md
@@ -0,0 +1,5 @@
+To run 17-GULP Assignment MEAN, use the command:
+
+```
+npm install && npm start
+```
--- 17_GULP_Assignment_MEAN/gulpfile.js
+++ 17_GULP_Assignment_MEAN/gulpfile.js
@@ -1,34 +1,38 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-
-// TODO: None of them are woking , this has to be fixed.
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const header = require('gulp-header');
+const footer = require('gulp-footer');
+const minify = require('gulp-minify');
 
 /* 
-    1. Write a program to coping data from one file to anotherusing gulp
+    1. Write a program to coping data from one file to another using gulp
 */
 gulp.task('copy_data', function () {
-    return gulp.src('workfiles/sample.txt')
-        .pipe(gulp.dest('*.copy.txt'))
+    return gulp.src('workfiles/foo.js', { base: 'workfiles/' })
+        .pipe(gulp.dest('dist/copied_workfiles/'));
 });
 
 
 /* 
-    2. Write a program to compile or debuging a fileusing gulp.
+    2. Write a program to compile or debuging a file using gulp.
 */
 gulp.task('compile', function () {
-    return gulp.src('./workfiles/concat.txt')
-        .pipe(concat('concatinated_files.txt'))
-        .pipe(gulp.dest('./workfiles/'));
+    return gulp.src('workfiles/foo.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('dist/compiled'));
 });
 
 
 /* 
-    3. Write a program to concate 2 or more filesusing gulp
+    3. Write a program to concate 2 or more files using gulp
 */
 gulp.task('concat_all', function () {
-    return gulp.src('./workfiles/concat.txt')
-        .pipe(concat('concatinated_files.txt'))
-        .pipe(gulp.dest('./workfiles/'));
+    return gulp.src('workfiles/*.js')
+        .pipe(concat('concat_all.js'))
+        .pipe(gulp.dest('dist'));
 });
 
 
@@ -36,20 +40,31 @@ gulp.task('concat_all', function () {
     4. Write a program to adding header or footer to all the files using gulp.
 */
 gulp.task('header_footer', function () {
-    return gulp.src('./workfiles/concat.txt')
-        .pipe(concat('concatinated_files.txt'))
-        .pipe(gulp.dest('./workfiles/'));
-});
+    var pkg = require('./package.json');
+    var banner = ['/**',
+        ' * <%= pkg.name %> - <%= pkg.description %>',
+        ' * @version v<%= pkg.version %>',
+        ' * @link <%= pkg.homepage %>',
+        ' * @license <%= pkg.license %>',
+        ' */',
+        ''].join('\n');
 
+    gulp.src('workfiles/*.js')
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(footer('\n\n//-------------------End of Program-------------------'))
+        .pipe(gulp.dest('dist'));
+});
 
 
 /* 
     5. Write a program to minification of files using gulp.
 */
 gulp.task('minify', function () {
-    return gulp.src('./workfiles/concat.txt')
-        .pipe(concat('concatinated_files.txt'))
-        .pipe(gulp.dest('./workfiles/'));
+    return gulp.src('workfiles/*.js')
+        .pipe(minify({
+            noSource: true
+        }))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['copy_data', 'compile', 'concat_all', 'header_footer', 'minify'])
